e8454a7d9b1844fa8b4d91a89134e43290553d52
Souheil CHELFOUH <trollfot@gmail.com>
2021-03-29 17:20:05 +0200

    Added messaging example

--- tests/test_dispatch.py
+++ tests/test_dispatch.py
@@ -43,3 +43,33 @@ def test_dispatch():
 
     app.notify('object_added', Item('car'))
     assert found == ['I have a car.']
+
+
+def test_messaging_example():
+
+    class User:
+        def __init__(self, **preferences):
+            self.preferences = preferences
+            self.events = []
+
+    class MessageDispatcher(Dispatcher):
+
+        def dispatch(self, user, message):
+            return user.preferences['messaging']
+
+    app = Application()
+    mc = MessageDispatcher()
+    app.subscribe('object_added')(mc)
+
+    @mc.subscribe('sms')
+    def sms_messaging(user, message):
+        user.events.append(f'I send you a SMS: {message}')
+
+    @mc.subscribe('Webpush')
+    def webpush_messaging(user, message):
+        user.events.append(f'I send you a webpush: {message}')
+
+
+    user = User(messaging='sms')
+    app.notify('object_added', user, message='This is a message')
+    assert user.events == ['I send you a SMS: This is a message']
